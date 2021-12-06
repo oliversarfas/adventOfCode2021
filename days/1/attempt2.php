dd39a874f14a7a3e7242f72f5f76143af8b373de
Oliver Sarfas <oliver@cleveregg.io>
2021-12-06 12:43:01 +0000

    Day 1 | Part 2

--- days/1/attempt2.php
+++ days/1/attempt2.php
@@ -1,3 +1,25 @@
 <?php
 
-$scanItems = json_decode(file_get_contents('./input1.json'));
+$scanItems = json_decode(file_get_contents('input1.json'));
+$groupIncreases = 0;
+
+function getSumForIndex(array $array, int $index): int
+{
+    if(!isset($array[$index], $array[$index+1], $array[$index+2])) {
+        return 0;
+    }
+    return $array[$index] + $array[$index+1] + $array[$index+2];
+}
+
+foreach ($scanItems as $key=>$value) {
+    $sumCurrentIndex = getSumForIndex($scanItems, $key);
+    $sumPreviousIndex = getSumForIndex($scanItems, $key - 1);
+    if(!$sumCurrentIndex || !$sumPreviousIndex) {
+        continue;
+    }
+    if($sumCurrentIndex > $sumPreviousIndex){
+        $groupIncreases++;
+    }
+}
+
+echo $groupIncreases;
